337436f80f5df510d374bcb076c674cf903ade4c
Jeffrey Mullins <jmullins@30and30.com>
2012-06-29 19:14:55 -0400

    Fixing postinstall script.

--- centos-5.7/kickstart/centos-5.7-x86-64-kickstart-net/isolinux/ks/postinstall/postinstall.sh
+++ centos-5.7/kickstart/centos-5.7-x86-64-kickstart-net/isolinux/ks/postinstall/postinstall.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "postinstall work has been moved to cfengine".
 
+POSTINSTALL_DIR="/root/postinstall"
+
+#Install Tech Residents repo
+cp $POSTINSTALL_DIR/repos/*.repo /etc/yum.repos.d
+
+#Check updates and update installed packages 
+yum clean all
+#yum -y check-update
+#yum -y update
+
+#Install python 2.7 since sysutils needs it and it's required for cfengine promises
+yum --disablerepo=* --enablerepo=tr -y install python27 python27-devel
+
+#Install cfengine and let it do the rest.
+yum --disablerepo=* --enablerepo=tr -y install cfengine-community

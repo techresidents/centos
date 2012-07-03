21318cfd0ca861aeab22103090377049150030ca
Jeffrey Mullins <jmullins@30and30.com>
2012-07-03 18:18:06 -0400

    Fixing ntpd setup in postinstall.sh

--- centos-5.7/kickstart/centos-5.7-x86-64-kickstart-net/isolinux/ks/postinstall/postinstall.sh
+++ centos-5.7/kickstart/centos-5.7-x86-64-kickstart-net/isolinux/ks/postinstall/postinstall.sh
@@ -3,7 +3,9 @@
 POSTINSTALL_DIR="/root/postinstall"
 
 #Sync time and date
-/sbin/ntpdate
+/sbin/chkconfig ntpd on
+/sbin/ntpdate pool.ntp.org
+/sbin/service ntpd start
 
 #Install Tech Residents repo
 cp $POSTINSTALL_DIR/repos/*.repo /etc/yum.repos.d
